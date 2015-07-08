d67a2f0a221d8cddfc41d2a1d9e4b0794af54ffe
Patrik Wehrli <weh@semabit.ch>
2015-07-08 18:52:36 +0200

    add german template for capistrano-maintenance

--- rails/capistrano.rb
+++ rails/capistrano.rb
@@ -22,7 +22,30 @@ after_bundle do
   puts '--> enabling capistrano-maintenance'
   gsub_file 'Capfile', "# require 'capistrano/rails/migrations'", "require 'capistrano/maintenance'"
 
+  unless no?('Add German maintenance Template? [yes]')
+    german_maintenance
+  end
+
   puts '--> removing comments from Capfile'
   gsub_file 'Capfile', /#.*\n/, ''
+end
 
+def german_maintenance
+  german_template = <<GERMAN_MAINTENANCE
+!!!
+%html
+  %head
+    %meta{:content => "text/html; charset=UTF-8", "http-equiv" => "Content-Type"}/
+    %title Unterhaltsarbeiten
+    %style
+      body { width: 400px; margin: 100px auto; font: 300 120% "OpenSans", "Helvetica Neue", "Helvetica", Arial, Verdana, sans-serif; }
+      h1 { font-weight: 300; }
+  %body
+    %h1 Unterhaltsarbeiten
+    %p= "Aufgrund von \#{ reason ? reason : 'Unterhaltsarbeiten' } ist das System zur Zeit nicht erreichbar (\#{ Time.now.strftime('%d.%m.%Y - %H:%M') })."
+    %p= "Das System wird \#{ deadline ? deadline : 'so bald als m&ouml;glich' } wieder verf&uuml;gbar sein."
+GERMAN_MAINTENANCE
+
+  create_file 'app/views/shared/maintenance.html.haml', german_template
+  insert_into_file 'config/deploy.rb', "\nset :maintenance_template_path, 'app/views/shared/maintenance.html.haml'\n", :after => "# set :keep_releases, 5\n"
 end
