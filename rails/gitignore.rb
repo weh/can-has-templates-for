15698431c73bd045950dbe36059cc6a9fbbe560b
Patrik Wehrli <weh@semabit.ch>
2015-07-08 20:14:51 +0200

    add config/database.yml to gitignore

--- rails/gitignore.rb
+++ rails/gitignore.rb
@@ -1,2 +1,4 @@
 puts '--> replace .gitignore'
 run 'wget https://raw.githubusercontent.com/github/gitignore/master/Rails.gitignore -O .gitignore'
+
+insert_into_file '.gitignore', "config/database.yml\n", :after => "config/secrets.yml\n"
